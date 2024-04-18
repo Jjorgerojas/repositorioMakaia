c4940a1f07389b580b3152c4ac99825152434315
Jjorgerojas <128833915+Jjorgerojas@users.noreply.github.com>
2024-04-18 09:31:26 -0500

    Add files via upload

---
+++ java/factorial/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int numerocalcular = 8;
+        int factorial  = 1;
+         for  ( int = 1; i =< numeroCalcular; i++){
+             factorial = factorial* i;
+        }
+}
+}
---
+++ java/practica1java/src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //saber cual de los dos numeros enteros es el mayor
+        //(tipo de dato) (nombre de la variable) = (valor)
+        // string  =jorge rojas
+        // int =>  9999999999
+        // long => sirve para almacer un numero muy grande
+        // double => almcena la parte entera mas el decimal
+        // bolean => solo almacena dos valores true, false
+
+        int numero1  = 10;
+        int numero2  = 17;
+
+        if (numero1 > numero2) {
+            System.out.println("el numero mayor es 1");
+        } else if (numero1 < numero2) {
+            System.out.println("el numero mayor es 2");
+        } else (numero1 === numero2) {
+            System.out.println("los numeros son iguales");
+    }
+    }
+}
